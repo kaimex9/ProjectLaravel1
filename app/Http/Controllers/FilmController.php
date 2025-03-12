0f8c32ca926cf36d1bdfba0e444bf3320d65b88a
kaimex9 <kaimexalabau@gmail.com>
2025-03-12 20:23:27 +0100

    idk

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class FilmController extends Controller
@@ -14,7 +15,8 @@ class FilmController extends Controller
     public static function readFilms(): array
     {
         $films = Storage::json('/public/films.json');
-        return $films;
+        $dbFilms = DB::table('films')->get();
+        return [$films, $dbFilms];
     }
     /**
      * List films older than input year 
